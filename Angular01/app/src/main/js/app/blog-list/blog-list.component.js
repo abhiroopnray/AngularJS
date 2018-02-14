67eb85e2301b49f6af98ae9a6e4a8c4f33e5a5f9
abhiroop.nandiray@mindtree.com <abhiroop.nandiray@mindtree.com>
2018-02-14 17:05:06 +0530

    Added controller with buttons

--- Angular01/app/src/main/js/app/blog-list/blog-list.component.js
+++ Angular01/app/src/main/js/app/blog-list/blog-list.component.js
@@ -4,5 +4,13 @@ angular.module ('blogList').
   controller('BlogListController', function($scope){
     console.log("blog-list controller");
     $scope.tittle = "Hi! There !!";
+    $scope.clicks = 0;
+    $scope.buttonClicked = function(){
+      console.log("Clicked button!");
+
+      $scope.clicks += 1;
+
+      $scope.tittle = "clicked " + $scope.clicks + " times!";
+    }
   });
   //component('blogList');
