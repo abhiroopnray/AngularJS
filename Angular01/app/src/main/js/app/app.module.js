b614903133baf545c2eeca212c240dd3aad55c7f
abhiroop.nandiray@mindtree.com <abhiroop.nandiray@mindtree.com>
2018-02-14 16:42:23 +0530

    added modules

--- Angular01/app/src/main/js/app/app.module.js
+++ Angular01/app/src/main/js/app/app.module.js
@@ -1,3 +1,3 @@
 'use strict'
 
-angular.module ('learn', []);
+angular.module ('learn', ['blogList']);
---
+++ Angular01/app/src/main/js/app/blog-list/blog-list.component.js
@@ -0,0 +1,8 @@
+'use strict'
+
+angular.module ('blogList').
+  controller('BlogListController', function($scope){
+    console.log("blog-list controller");
+    $scope.tittle = "Hi! There !!";
+  });
+  //component('blogList');
